563f3a081d152948e9f793f6b122a68f26ad35e5
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-26 11:52:37 +0300

    Create task create/update view

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from todo_list.views import TagListView, TaskListView, TagCreateView, TagUpdateView
+from todo_list.views import TagListView, TaskListView, TagCreateView, TagUpdateView, TaskCreateView, TaskUpdateView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("/create", TaskCreateView.as_view(), name="task-create"),
+    path("/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
 ]
 
 app_name = "todo_list"
--- todo_list/views.py
+++ todo_list/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
+from todo_list.forms import TaskForm
 from todo_list.models import Tag, Task
 
 
@@ -25,3 +26,15 @@ class TagUpdateView(generic.UpdateView):
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_list:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_list:task-list")
