468ff37dfdbc6a679d2a7f4e9a305aac4290fb06
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-26 10:49:52 +0300

    Add models Tag and Task

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField()
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering = ["-completed", "-created_at"]
+
+    def __str__(self):
+        return self.content
