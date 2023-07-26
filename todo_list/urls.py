cc5e4df2143a408747ba09be53e671dd2cf4fbea
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-26 11:41:27 +0300

    Create tag create/update view

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from todo_list.views import TagListView, TaskListView
+from todo_list.views import TagListView, TaskListView, TagCreateView, TagUpdateView
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
 ]
 
 app_name = "todo_list"
--- todo_list/views.py
+++ todo_list/views.py
@@ -8,10 +8,20 @@ from todo_list.models import Tag, Task
 class TagListView(generic.ListView):
     model = Tag
     paginate_by = 5
-    success_url = reverse_lazy("togo_list:tag-list")
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:tag-list")
 
 
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
-    success_url = reverse_lazy("togo_list:task-list")
