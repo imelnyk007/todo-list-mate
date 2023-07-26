ef5224acf89aa1ec358daba1c140f34dbb8f1dca
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-26 14:05:39 +0300

    Set default value for tags field for Task

--- todo_list/models.py
+++ todo_list/models.py
@@ -14,7 +14,7 @@ class Tag(models.Model):
 class Task(models.Model):
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    completed = models.BooleanField()
+    completed = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag)
 
     class Meta:
