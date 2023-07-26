9ae9288744582784cd681e041d45f44ba45926b5
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-26 11:10:34 +0300

    Create views for tag-list and task-list

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from todo_list.models import Tag, Task
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 5
+    success_url = reverse_lazy("togo_list:tag-list")
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 5
+    success_url = reverse_lazy("togo_list:task-list")
