b447d7c141b91a0d09818c4a74f938c56a77b4bf
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-26 10:58:04 +0300

    Update list_display for model Task at admin page

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from todo_list.models import Tag, Task
 
+admin.site.unregister(Group)
+
 admin.site.register(Tag)
-admin.site.register(Task)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("content", "created_at", "completed")
